b583ec9ed629f7eff4de859c86493aa0a24431ac
Clement Mukhale <xenobyte012@gmail.com>
2026-05-14 01:36:30 +0200

    delete function for category

--- src/components/CategoryComponent.jsx
+++ src/components/CategoryComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { NotesContext } from "../App";
 import { useContext } from "react";
+import circle_img from "../images/circle-solid.png";
+import selected_img from "../images/circle-check-solid.png";
 
 function CategoryComponent(props) {  
   const {
@@ -8,28 +10,24 @@ function CategoryComponent(props) {
     listOfCategorys
   } = useContext(NotesContext)
 
-  let groupCategory = Object.groupBy(notes, (note) => note.category);
-  let countGroupedCategory = Object.entries(groupCategory).map(([category, items]) => ({
-    category,
-    count: items.length,
-  }));
-  const results = countGroupedCategory.map(item => {
-    return item === 'katlego' ? item.count : 0;
-    
-  })
-  //console.log(results)
 
-  const l = countGroupedCategory.find(find => props.len == find)
-    //const resultss = l.map(item => item);
-  //-vconsole.log(l);
 
   return (
-    <div className=" justify-between flex flex-row bg-gray-900 border-rounded text-white my-2 rounded-2xl p-3 font-sans ">
-      <div>
-        <span>{props.category}</span>
-      </div>
-      <div>
-        <span>{props.len === "all" ? (notes.length)  : ''}</span>
+    <div className="  bg-gray-900 border-rounded text-white my-2 rounded-2xl p-3 font-sans ">
+      <div className="flex flex-row  justify-between">
+        <div>
+          <span>{props.category}</span>
+        </div>
+        <div>
+          <img
+            src={circle_img}
+            alt="circle"
+            className="w-7"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/SearchComponents.jsx
+++ src/components/SearchComponents.jsx
@@ -1,44 +1,45 @@
-import React from 'react'
-import { NotesContext } from '../App';
-import { useContext,  } from "react";
+import React from "react";
+import { NotesContext } from "../App";
+import { useContext } from "react";
 
 function SearchComponents() {
-    const {
-      isSearch,
-      setIsSearch,
-      search,
-       setSearch
-    } = useContext(NotesContext);
+  const { isSearch, setIsSearch, search, setSearch } = useContext(NotesContext);
 
-
-    function handleSearch(event) {
-      setSearch(event.target.value)
-      setIsSearch(true)
-      console.log(search)
-    }
-    return (
-      <div>
-        <form>
-          <div className='flex flex-row items-center justify-between  '>
-            <div>
+  function handleSearch(event) {
+    setSearch(event.target.value);
+    setIsSearch(true);
+    console.log(search);
+  }
+  return (
+    <div>
+      <form>
+        <div className="flex flex-row items-center justify-between  ">
+          <div className="w-full">
             <input
-            type="Seach For Notes"
-            value={search}
-            placeholder="Search"
-            className="bg-gray-900 w-[300px] text-white font-2xl rounded-full pl-5  p-3 px-4 font-lg font-sans outline-none "
-            value={search}
-            onChange={handleSearch}
-            onClick={() => setIsSearch(false)}
-          />
-            </div>
-            <div className={`${!isSearch ? "hidden" : "block"}`}>
-              <p className='text-blue-700 text-xl' onClick={() => {setIsSearch(false); setSearch('')}}>cancel</p>
-            </div>
+              type="Seach For Notes"
+              value={search}
+              placeholder="Search"
+              className="bg-gray-900 w-full text-white font-2xl rounded-full pl-5  p-3 px-4 font-lg font-sans outline-none "
+              value={search}
+              onChange={handleSearch}
+              onClick={() => setIsSearch(false)}
+            />
           </div>
-          
-        </form>
-      </div>
-    );
+          <div className={`${!isSearch ? "hidden" : "block"} pl-2`}>
+            <p
+              className="text-blue-700 text-xl"
+              onClick={() => {
+                setIsSearch(false);
+                setSearch("");
+              }}
+            >
+              cancel
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SearchComponents
+export default SearchComponents;
